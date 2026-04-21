0338bb7cf92b834273b4b24df3a86c8089ff07cc
Sunitha Anilkumar <sunitha181288@gmail.com>
2026-04-21 16:03:43 +0800

    CORS error

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,6 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.chat import router as chat_router
